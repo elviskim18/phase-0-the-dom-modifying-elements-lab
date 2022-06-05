4d391c93d0991e455a0100900789c46f4876eff7
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-05 14:49:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const elementMain = document.getElementById("main")
+elementMain.remove();
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+//newHeader.setAttribute(victory)
+newHeader.textContent = "YOUR-NAME is the champion"
+document.body.append(newHeader);
